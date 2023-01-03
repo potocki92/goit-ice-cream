8d28d5da2b2a4198a8ac80eff1e55c59b9dae3c5
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-01-03 14:06:14 +0000

    Deploying to gh-pages from @ potocki92/goit-ice-cream@aa05a1e87da9904ee6b4b5ee18af951f43fe58fc 🚀

--- src.d8ea1b01.js
+++ src.d8ea1b01.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-ice-cream/src.eb26d8fc.js.map
+//# sourceMappingURL=/goit-ice-cream/src.d8ea1b01.js.map
